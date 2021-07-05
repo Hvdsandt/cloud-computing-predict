4637cb9f25786b9434a6bb2f527048688a31b687
Hannes <Hvdsandt@users.noreply.github.com>
2021-07-05 19:21:42 +0200

    Update contact_me.js
    changed url

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://5ki31pai0b.execute-api.eu-west-1.amazonaws.com",
+                url: "https://5ki31pai0b.execute-api.eu-west-1.amazonaws.com/explore-predict-lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
