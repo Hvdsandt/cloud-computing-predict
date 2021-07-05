72d82fa2c7f76f6d35c758982cb4cf2bc33e15ee
Hannes <Hvdsandt@users.noreply.github.com>
2021-07-05 15:33:16 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "http://hvdsandt-predict.com",
+                url: "https://mpru1j5gk3.execute-api.eu-west-1.amazonaws.com/explore-predict-lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
